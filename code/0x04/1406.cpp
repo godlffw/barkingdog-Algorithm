0d61b5d0df08ef934b745aa4414856df94769b08
godlffw <godlfgw@gmail.com>
2026-08-11 12:09:15 +0900

    Create 1406.cpp
    \

---
+++ code/0x04/1406.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <list>
+
+using namespace std;
+
+int main() {
+    string word;
+    cin >> word;
+    list<char> lst;
+
+    for (char c : word) {
+        lst.push_back(c);
+    }
+    auto it = lst.end();
+    int num;
+    cin >> num;
+    cin.ignore();
+    for (int i = 0; i < num; i++) {
+        string cmd;
+        getline(cin, cmd);
+        if (cmd[0] == 'P') {
+            it = lst.insert(it, cmd[2]);
+            it++;
+        }
+        else if (cmd[0] == 'L') {
+            if (it != lst.begin()) --it;
+        }
+        else if (cmd[0] == 'D') {
+            if (it != lst.end()) ++it;
+        }
+        else if (cmd[0] == 'B') {
+            if (it != lst.begin()) {
+                auto tmp = it;
+                tmp--;
+                it = lst.erase(tmp);
+            }
+        }
+    }
+
+    for (char c : lst) {
+        cout << c;
+    }
+    
+}
